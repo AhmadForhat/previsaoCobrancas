cba1e23d4f718432792c61616973f9c18cb09ff1
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-08 09:12:15 -0300

    comentários

--- src/lambdas/previsaoCobrancas.js
+++ src/lambdas/previsaoCobrancas.js
@@ -5,25 +5,28 @@ const objectToArray = require('@ziro/object-array')
 const main = require('../templates/main')
 const optionsGet = require('../utils/optionsSheetBatch')
 const calcPrevisoes = require('../utils/calcPrevisoes')
-const sendSheetOptions = require('../utils/optionsSheetPost')
+// const sendSheetOptions = require('../utils/optionsSheetPost') // Requisição para POST googleSheets
 require('dotenv').config()
 
 const sendEmail = async () => {
     try {
+        // Requisição
         const dataSheet = await axios(optionsGet(['sendJavaScript!A:D']))
         const baseSheet = await arrayToObject(dataSheet.data.valueRanges[0])
         const resultsPrevisoes = calcPrevisoes(baseSheet)
+        // Resultado em Objeto
         const resultPrevisoesData = resultsPrevisoes.map(item => {
             return {
                 proximaCobranca: moment.utc(item.proximaCobranca).local().format('DD/MM/YYYY'),
                 cobrancasAtrasadas: moment.utc(item.cobrancasAtrasadas).local().format('DD/MM/YYYY')
             }})
+        // Resultado em Array
         const resultSheet = objectToArray([resultPrevisoesData])
-        const sendSheet = await axios(sendSheetOptions(resultSheet,'Boletos!AA1'))
-        console.log(sendSheet)
+        // Função POST Sheets
+        // const sendSheet = await axios(sendSheetOptions(resultSheet,'Boletos!AA1'))
         return {
             statusCode: 200,
-            body: JSON.stringify(baseSheet)
+            body: JSON.stringify(resultSheet)
         }
     } catch (error) {
         console.log(error)
