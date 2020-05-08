a2518ce4059033af18d69450686cca1bd0d27577
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-08 08:56:16 -0300

    configurações inicias netlify

---
+++ src/templates/main.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const middy = require('@middy/core')
+const jsonBodyParser = require('@middy/http-json-body-parser')
+const { preflight } = require('@ziro/middleware')
+const { auth } = require('@ziro/middleware')
+const { errorHandler } = require('@ziro/middleware')
+const { cors } = require('@ziro/middleware')
+
+const lambda = handler =>
+    middy(handler)
+        .use(preflight)
+        .use(auth)
+        .use(jsonBodyParser())
+        .use(errorHandler)
+        .use(cors)
+
+module.exports = lambda
